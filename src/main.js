03979d652572a37add6458cdb1eeabbf1118c6b3
Michael Wu <michael.wu@emory.edu>
2020-08-12 21:27:07 -0400

    Setup

--- src/main.js
+++ src/main.js
@@ -1,8 +1,30 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from "vue";
+import App from "./App.vue";
+import VueRouter from "vue-router";
+import store from "./store";
+import axios from "axios";
+import Home from "@/views/Home.vue";
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
+Vue.use(VueRouter);
 
-new Vue({
-  render: h => h(App),
-}).$mount('#app')
+const routes = [{ name: "Home", path: "/", component: Home }];
+
+const router = new VueRouter({
+    routes,
+    mode: "history"
+});
+
+export let client;
+(async function() {
+    const response = await axios.get("/appSettings.json");
+    const appSettings = response.data;
+    client = axios.create({
+        baseURL: appSettings.apiUrl
+    });
+    new Vue({
+        router,
+        store,
+        render: h => h(App)
+    }).$mount("#app");
+})();
